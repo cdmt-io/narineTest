d1ec499de9a2358b986e27bac2ae2347b0a25ed2
randyunger <randy.unger@gmail.com>
2019-03-13 20:38:51 -0700

    Basics customized - name, photo, contacts

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
-  copyright: '© All rights reserved.',
+  url: 'https://randleunger.com',
+  title: 'Randy Unger - Personal Site',
+  subtitle: 'Projects, resume, about me',
+  copyright: '© 2019 All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
   googleAnalyticsId: 'UA-73379983-2',
@@ -16,23 +16,23 @@ module.exports = {
     {
       label: 'About me',
       path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
     }
+    // ,{
+    //   label: 'Contact me',
+    //   path: '/pages/contacts'
+    // }
   ],
   author: {
-    name: 'John Doe',
-    photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    name: 'Randle Unger',
+    photo: '/randyunger.png',
+    bio: 'Developer, Product Manager, Marketer',
     contacts: {
-      email: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '#',
-      vkontakte: '#'
+      github: 'randyunger',
+      email: 'randy.unger@gmail.com',
+      // telegram: '#',
+      twitter: '',
+      // rss: '#',
+      // vkontakte: '#'
     }
   }
 };
